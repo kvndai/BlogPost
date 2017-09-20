d28cb796581fb5be9b8886b03bf05c36a16082f0
kvndai <kevin.t.dai@gmail.com>
2017-09-19 20:35:41 -0700

    creating data

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,7 +4,6 @@ import { FETCH_POSTS } from "../actions/index";
 export default function(state = {}, action) {
   switch (action.type) {
   case FETCH_POSTS:
-    console.log(action.payload.data); // [post1, post2]
     return _.mapKeys(action.payload.data, "id"); // {4: post4}
   default:
     return state;
