062a06316cc9431194e89c67de8219c2b6e07a6b
kvndai <kevin.t.dai@gmail.com>
2017-09-20 14:33:22 -0700

    id params/single post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const ROOT_URL = "http://reduxblog.herokuapp.com/api/";
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=paperclip825";
 
 export const FETCH_POSTS = "fetch_posts";
+export const CREATE_POST = "create_post"
+export const FETCH_POST = "fetch_post"
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -12,4 +14,23 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(values, callback){
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id){
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -21,7 +24,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values)
+    this.props.createPost(values, () => {
+      this.props.history.push('/')
+    })
   }
 
   render() {
@@ -47,6 +52,7 @@ class PostsNew extends Component {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link className="btn btn-danger" to="/">Cancel</Link>
       </form>
     );
   }
@@ -74,4 +80,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: "PostsNewForm"
-})(PostsNew);
+})(
+  connect(null,{ createPost })(PostsNew)
+);
